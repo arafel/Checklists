d00171e3031dbaaf4d3784b6a31c6694b7ef4ad4
Paul Walker <paul@blacksun.org.uk>
2017-07-12 21:40:25 +0100

    Add ChecklistItem class, but still using instance variables rather than array.

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Paul Walker on 12/07/2017.
+//  Copyright © 2017 Paul Walker. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
